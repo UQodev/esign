9c95310a3af80cbcb7bc41c4aa79411ea2e6e373
Bajindul <ukisaw296@gmail.com>
2024-12-02 23:25:10 +0700

    membuat fungsi dan implementasi fungsi login register

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:esign/core/error/failures.dart';
+import 'package:esign/domain/entities/user.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failure, void>> signIn(String email, String password);
-  Future<Either<Failure, void>> signOut();
+  Future<Either<Failure, User>> register(
+      String name, String email, String password);
+  Future<Either<Failure, User>> login(String email, String password);
+  Future<Either<Failure, void>> logout();
 }
--- lib/domain/repositories/auth_repository_impl.dart
+++ lib/domain/repositories/auth_repository_impl.dart
@@ -1,28 +1,91 @@
+import 'dart:convert';
+
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:dartz/dartz.dart';
 import 'package:esign/core/error/failures.dart';
-import 'package:esign/data/datasources/supabase_datasources.dart';
+import 'package:esign/domain/entities/user.dart';
 import 'package:esign/domain/repositories/auth_repository.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' hide User;
+import 'package:crypto/crypto.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
-  final SupabaseDataSources dataSource;
+  final SupabaseClient supabase;
+  final String jwtSecret = 'jwtSecret';
 
-  AuthRepositoryImpl(this.dataSource);
+  AuthRepositoryImpl(this.supabase);
+
+  String _hashPassword(String passwrd) {
+    var bytes = utf8.encode(passwrd);
+    return sha256.convert(bytes).toString();
+  }
+
+  String _generateToken(String id) {
+    final jwt = JWT({
+      'id': id,
+      'iat': DateTime.now().millisecondsSinceEpoch,
+    });
+    return jwt.sign(SecretKey(jwtSecret));
+  }
+
   @override
-  Future<Either<Failure, void>> signIn(String email, String password) async {
+  Future<Either<Failure, User>> register(
+      String name, String email, String password) async {
     try {
-      final response = await dataSource.supabase.auth.signInWithPassword(
-        email: email,
-        password: password,
+      final hashedPassword = _hashPassword(password);
+
+      final response = await supabase
+          .from('users')
+          .insert({
+            'name': name,
+            'email': email,
+            'password': hashedPassword,
+          })
+          .select()
+          .single();
+
+      final user = User(
+        id: response['id'],
+        name: response['name'],
+        email: response['email'],
+        token: _generateToken(response['id']),
+        createdAt: DateTime.parse(response['created_at']),
+        updatedAt: DateTime.parse(response['updated_at']),
       );
-      return Right(response);
+
+      return Right(user);
     } catch (e) {
       return Left(ServerFailure());
     }
   }
 
-  Future<Either<Failure, void>> signOut() async {
+  @override
+  Future<Either<Failure, User>> login(String email, String password) async {
     try {
-      await dataSource.supabase.auth.signOut();
+      final hashedPassword = _hashPassword(password);
+
+      final response = await supabase.from('users').select().match({
+        'email': email,
+        'password': hashedPassword,
+      }).single();
+
+      final user = User(
+        id: response['id'],
+        name: response['name'],
+        email: response['email'],
+        token: _generateToken(response['id']),
+        createdAt: DateTime.parse(response['created_at']),
+        updatedAt: DateTime.parse(response['updated_at']),
+      );
+
+      return Right(user);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
+  }
+
+  Future<Either<Failure, void>> logout() async {
+    try {
+      await supabase.auth.signOut();
       return const Right(null);
     } catch (e) {
       return Left(ServerFailure());
