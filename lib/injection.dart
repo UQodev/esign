3cff4c02cdfd932f9907a487739b8768306e3cae
Bajindul <ukisaw296@gmail.com>
2024-12-03 21:53:29 +0700

    untuk mengelola dependensi agar tidak saling ketergantungan langsung

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,10 +1,19 @@
-import 'package:esign/data/datasources/supabase_datasources.dart';
+import 'package:esign/domain/repositories/auth_repository.dart';
+import 'package:esign/domain/repositories/auth_repository_impl.dart';
+import 'package:esign/presentation/bloc/auth/authBloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> configureDependencies() async {
-  getIt.registerLazySingleton<SupabaseDataSources>(
-    () => SupabaseDataSourceImpl(),
+  getIt.registerLazySingleton<SupabaseClient>(
+    () => Supabase.instance.client,
   );
+
+  getIt.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(getIt<SupabaseClient>()),
+  );
+
+  getIt.registerFactory(() => AuthBloc(getIt<AuthRepository>()));
 }
