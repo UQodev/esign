b0f9f2c67eb3f172dcf128c918e012a2284be9f0
Bajindul <ukisaw296@gmail.com>
2024-12-04 23:50:20 +0700

    merombak file login untuk memanggil beberapa fungsi login yang sudah dibuat secara terpisah

---
+++ lib/presentation/pages/auth/login_page.dart
@@ -0,0 +1,140 @@
+import 'package:esign/injection.dart';
+import 'package:esign/presentation/bloc/auth/authBloc.dart';
+import 'package:esign/presentation/bloc/auth/authEvent.dart';
+import 'package:esign/presentation/bloc/auth/authState.dart';
+import 'package:esign/presentation/layouts/baseLayout.dart';
+import 'package:esign/presentation/pages/auth/register_page.dart';
+import 'package:esign/presentation/pages/home_page.dart';
+import 'package:esign/presentation/widgets/auth/login_form.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _handleAuthState(BuildContext context, AuthState state) {
+    if (state is AuthLoading) {
+      setState(() => _isLoading = true);
+    } else {
+      setState(() => _isLoading = false);
+    }
+
+    if (state is AuthSuccess) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const MyHomePage()));
+    } else if (state is AuthError) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(state.message)));
+    }
+  }
+
+  void _handleLogin() {
+    if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Semua field harus diisi!')));
+      return;
+    }
+
+    context.read<AuthBloc>().add(
+          LoginEvent(_emailController.text, _passwordController.text),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) => BaseLayout(
+        showAppBar: false,
+        showBackButton: false,
+        body: BlocProvider(
+          create: (context) => getIt<AuthBloc>(),
+          child: BlocConsumer<AuthBloc, AuthState>(
+            listener: _handleAuthState,
+            builder: (context, state) => SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Login',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 40),
+                    LoginForm(
+                      emailController: _emailController,
+                      passwordController: _passwordController,
+                      obscurePassword: _obscurePassword,
+                      onTogglePassword: () =>
+                          setState(() => _obscurePassword = !_obscurePassword),
+                      isLoading: _isLoading,
+                      onLogin: _handleLogin,
+                    ),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        // Handle forgot password
+                      },
+                      child: const Text('Lupa Password?'),
+                    ),
+                    const SizedBox(height: 20),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              const TextSpan(
+                                text: 'Belum punya akun? ',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              TextSpan(
+                                text: 'Daftar di sini',
+                                style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const RegisterPage(),
+                                      ),
+                                    );
+                                  },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+}
