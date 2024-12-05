9cf944a6ff3e4de86777cf4aff4261343fb4e2ba
Bajindul <ukisaw296@gmail.com>
2024-12-05 19:41:29 +0700

    Add auth_remote for communicate & handle error with supabase

---
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,79 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class AuthRemoteDataSource {
+  Future<Map<String, dynamic>> register(
+      String name, String email, String password);
+  Future<Map<String, dynamic>> login(String email, String password);
+  Future<Map<String, dynamic>> checkAuth();
+  Future<void> logout(String? rememberToken);
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabase;
+
+  AuthRemoteDataSourceImpl({required this.supabase});
+
+  @override
+  Future<Map<String, dynamic>> register(
+      String name, String email, String password) async {
+    try {
+      final response = await supabase
+          .from('users')
+          .insert({
+            'name': name,
+            'email': email,
+            'password': password,
+          })
+          .select()
+          .single();
+
+      if (response == null) {
+        throw Exception('Registration failed: $response');
+      }
+      return response;
+    } catch (e) {
+      throw Exception('Register failed : $e');
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    try {
+      final response = await supabase.from('users').select().match({
+        'email': email,
+        'password': password,
+      }).single();
+
+      return response;
+    } catch (e) {
+      throw Exception('Login failed: $e');
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> checkAuth() async {
+    try {
+      final response = await supabase
+          .from('users')
+          .select()
+          .not('remember_token', 'is', null)
+          .single();
+
+      return response;
+    } catch (e) {
+      throw Exception('Auth check failed: $e');
+    }
+  }
+
+  @override
+  Future<void> logout(String? rememberToken) async {
+    try {
+      if (rememberToken != null) {
+        await supabase.from('users').update({'remember_token': null}).match(
+            {'remember_token': rememberToken});
+      }
+    } catch (e) {
+      throw Exception('Logout failed: $e');
+    }
+  }
+}
