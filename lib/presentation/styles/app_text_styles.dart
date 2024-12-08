b73ce18c403ac2101435bfcb74e5a6e12a896820
Bajindul <ukisaw296@gmail.com>
2024-12-08 19:47:52 +0700

    Move to folder core/theme

--- lib/presentation/styles/app_text_styles.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTextStyles {
-  static const TextStyle input = TextStyle(
-      fontWeight: FontWeight.w400,
-      fontStyle: FontStyle.normal,
-      fontSize: 15,
-      color: Color(0xFF1D242B));
-}
