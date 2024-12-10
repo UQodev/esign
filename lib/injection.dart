9470beb90a0fd93f9552b7a912c90045929123b8
Bajindul <ukisaw296@gmail.com>
2024-12-10 22:07:12 +0700

    register profile dependencies

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,10 +1,14 @@
 import 'package:esign/data/datasources/auth_remote_datasource.dart';
+import 'package:esign/data/datasources/profile_remote_datasource.dart';
 import 'package:esign/data/datasources/signature_remote_datasource.dart';
+import 'package:esign/data/repositories/profile_repository_impl.dart';
 import 'package:esign/data/repositories/signature_repository_impl.dart';
 import 'package:esign/domain/repositories/auth_repository.dart';
 import 'package:esign/data/repositories/auth_repository_impl.dart';
+import 'package:esign/domain/repositories/profile_repository.dart';
 import 'package:esign/domain/repositories/signature_repository.dart';
 import 'package:esign/presentation/bloc/auth/auth_bloc.dart';
+import 'package:esign/presentation/bloc/profile/profile_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -30,4 +34,13 @@ Future<void> configureDependencies() async {
 
   getIt.registerLazySingleton<SignatureRepository>(
       () => SignatureRepositoryImpl(getIt()));
+
+  getIt.registerLazySingleton<ProfileRemoteDataSource>(
+      () => ProfileRemoteDataSourceImpl(supabase: getIt()));
+
+  getIt.registerLazySingleton<ProfileRepository>(
+    () => ProfileRepositoryImpl(remoteDataSource: getIt()),
+  );
+
+  getIt.registerFactory(() => ProfileBloc(getIt()));
 }
