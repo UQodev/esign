8a68d7469c4d8ecb1f4dda70629eb7fdcc680962
Bajindul <ukisaw296@gmail.com>
2025-01-04 12:17:33 +0700

    rename route

--- lib/core/routes/routes_name.dart
+++ lib/core/routes/routes_name.dart
@@ -2,6 +2,6 @@ class RouteName {
   static const String login = '/login';
   static const String home = '/';
   static const String profile = '/profile';
-  static const String documents = '/documents';
+  static const String documents = '/document/list';
   static const String documentSign = '/document/sign';
 }
