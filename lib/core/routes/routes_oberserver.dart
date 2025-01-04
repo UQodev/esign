638c4602fe1d7e431c4cdb93f5aab0713888cd82
Bajindul <ukisaw296@gmail.com>
2025-01-04 12:16:25 +0700

    add new debug print current location page

--- lib/core/routes/routes_oberserver.dart
+++ lib/core/routes/routes_oberserver.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CustomRouteObserver extends NavigatorObserver {
+  String? _currentRoute;
+
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    print('🚀 PUSHED: ${route.settings.name}');
-    print('⬅️ FROM: ${previousRoute?.settings.name}');
+    _currentRoute = route.settings.name;
+    print('🚀 PUSHED: ${route.settings.name ?? 'No Name'}');
+    print('⬅️ FROM: ${previousRoute?.settings.name ?? 'No Name'}');
+    print('📍 Current Route: $_currentRoute');
   }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    _currentRoute = previousRoute?.settings.name;
     print('🔙 POPPED: ${route.settings.name}');
     print('➡️ TO: ${previousRoute?.settings.name}');
+    print('📍 Current Route: $_currentRoute');
   }
 
   @override
   void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
+    _currentRoute = newRoute?.settings.name;
     print('🔄️ REPLACED: ${newRoute?.settings.name}');
     print('📤 TO: ${oldRoute?.settings.name}');
+    print('📍 Current Route: $_currentRoute');
   }
 }
